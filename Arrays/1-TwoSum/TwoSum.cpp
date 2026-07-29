727d157e3cd434a12ca2da49f84baa9fdc14d71e
KaptainKoopa1309 <vrzorawarsingh@gmail.com>
2026-07-30 04:16:05 +0530

    1) TwoSum Problem Solution using hash map

---
+++ Arrays/1-TwoSum/TwoSum.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> DaMap;
+        int n = nums.size();
+
+        for(int i=0; i<n;i++){
+            DaMap[nums[i]]= i;
+        }
+        
+        for(int i=0; i<n;i++){
+            int complement = target - nums[i];
+            cout<< DaMap.count(complement) << DaMap[complement];
+            if(DaMap.count(complement) && DaMap[complement] != i){
+                return {i, DaMap[complement]};
+            }
+
+        }
+        return {};
+    }
+
+};
+
+int main(){
+    Solution sol;
+    vector<int> numbers = {2,7,11,15};
+    int target = 9;
+    vector<int> Answer;
+    Answer = sol.twoSum(numbers, target);
+    cout<<"The Two numbers that add up to"<<target<<"are:"<< numbers[Answer[0]] << "" << numbers[Answer[1]];
+    return 0;
+}
+
--- README.md
+++ README.md
@@ -9,3 +9,5 @@ This Repository has all my solved C++ DSA Problems, arranged via topics as follo
 - Hash Maps 
 - Binary Search
 - Sorting Algorithms
+
+Alongside each solution, i have included my thought process for why I have applied given methods, as well as alternative solutions alongside their complexity analysis.
